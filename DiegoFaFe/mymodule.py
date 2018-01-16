7ac991fae87c9d15f19c4905e713ce48224ebbb9
FaFeComputer <diegofafe@gmail.com>
2018-01-16 21:19:41 +0100

    After kata

--- DiegoFaFe/mymodule.py
+++ DiegoFaFe/mymodule.py
@@ -1,4 +1,29 @@
 # -*- coding: utf-8 -*-
 
-def my_function():
-    pass
+from expects import *
+
+class FizzBuzz:
+    num1 = 3
+    num2 = 5
+    def __init__(self):
+        pass
+
+    def calculate(self, number):
+        if number == 1:
+            return 1
+        elif number ==2:
+            return 2
+        else:
+             return 'Fizz'
+
+def return_1_if_number_is_1():
+    x = FizzBuzz()
+    expect(x.calculate(1) == 1).to(be_true)
+
+def return_2_if_number_is_2():
+    x = FizzBuzz()
+    expect(x.calculate(2) == 2).to(be_true)
+
+def return_Fizz_if_number_is_3():
+    x = FizzBuzz()
+    expect(x.calculate(3) == 'Fizz').to(be_true)
--- DiegoFaFe/mymodule_spec.py
+++ DiegoFaFe/mymodule_spec.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from expects import *
+import mymodule
 
-
-with describe('Fizzbuzz'):
-    with context('when xx'):
-        with it('yy'):           
-            expect(True).to(be_true)
-            
+with description('FizzBuzz'):
+    with it('inputs 1 returns 1'):
+        mymodule.return_1_if_number_is_1()
+    with it('inputs 2 returns 2'):
+        mymodule.return_2_if_number_is_2()
+    with it('inputs 3 returns Fizz'):
+        mymodule.return_Fizz_if_number_is_3()
